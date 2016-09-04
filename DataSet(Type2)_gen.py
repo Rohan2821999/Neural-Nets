9847833107de7c29e2e9c16ed3f5dd0af1abb7dd
Rohan2821999 <rohanhundia@gmail.com>
2016-09-04 13:37:31 -0700

    Type 2 Data for ANN MathLearning

---
+++ DataSet(Type2)_gen.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+array = np.zeros(19)
+operation_array = [1,0]
+new_array = []
+
+
+for i in xrange(1,10):
+    for j in xrange(10,19):
+        if i >= (j-9):
+            for k in xrange(2):
+                array[0:i] = 1
+                array[9:j] = 1
+                array[18] = operation_array[k]
+                new_array.append(array)
+                array = np.zeros(19)
+        if i < (j-9):
+            array[0:i] = 1
+            array[9:j] = 1
+            array[18] = operation_array[0]
+            new_array.append(array)
+            array = np.zeros(19)
+            
+
+print(new_array)
+#print(len(new_array))
+
+
+    
